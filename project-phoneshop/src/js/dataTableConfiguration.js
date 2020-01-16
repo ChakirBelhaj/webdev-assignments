5668f313ca649ae909c79a00b4c199adf8c37c10
Chakir Belhaj <C.belhaj@creates.nl>
2020-01-16 13:38:33 +0100

    reconstructing maps

---
+++ project-phoneshop/src/js/dataTableConfiguration.js
@@ -0,0 +1,18 @@
+function dataTablePluginConfiguration(){
+    $("#phonesTable").DataTable({
+        responsive: true,
+        columns: [{ data: "image" }, { data: "brand" }, { data: "model" }, { data: "os" }, { data: "screensize" }],
+        columnDefs: [
+            {
+                targets: 0,
+                render: function(data) {
+                    if (data == '<input value="t" required="" type="text" name="image">') {
+                        return data;
+                    }
+                    return '<img class="imageInTable" src="' + data + '">';
+                }
+            }
+        ],
+        lengthMenu: [5, 10, 20]
+    });
+}
--- project-phoneshop/src/js/phonesController.js
+++ project-phoneshop/src/js/phonesController.js
@@ -1,37 +1,44 @@
-$(document).ready(function() {
+//this function is triggered onsubmit
+function formOnSubmit(){
     $("#phonesForm").submit(function(e) {
         e.preventDefault();
         createPhone();
     });
+}
 
-    $(document).ready(function() {
-        $("#phonesTable").DataTable({
-            responsive: true,
-            columns: [{ data: "image" }, { data: "brand" }, { data: "model" }, { data: "os" }, { data: "screensize" }],
-            columnDefs: [
-                {
-                    targets: 0,
-                    render: function(data) {
-                        if (data == '<input value="t" required="" type="text" name="image">') {
-                            return data;
-                        }
-                        return '<img class="imageInTable" src="' + data + '">';
-                    }
-                }
-            ],
-            lengthMenu: [5, 10, 20]
-        });
-        getPhoneData();
+// after submitting this function adds the formdata to the api through a post request and creates a new tablerow
+function createPhone() {
+    var formData = $("#phonesForm").serializeArray();
+    data = {
+        image: formData[1].value,
+        brand: formData[2].value,
+        model: formData[3].value,
+        os: formData[4].value,
+        screensize: formData[5].value
+    };
+
+    $.ajax({
+        url: "https://wt.ops.labs.vu.nl/api20/2b9115a4",
+        data: data,
+        method: "POST",
+        success: function(result) {
+            addPhoneToTable(data);
+            alert(data.brand + " successfully added");
+        },
+        error: function(error) {
+            alert(error.responseText);
+        }
     });
-});
+}
 
-function resetData() {
+//calls an ajax get request to reset the data in
+function resetTableData() {
     $.ajax({
         url: "https://wt.ops.labs.vu.nl/api20/2b9115a4/reset",
         method: "GET",
         success: function(result) {
             $("#phonesTable").DataTable().clear().draw();
-            getPhoneData();
+            getPhoneDataFromApi();
             alert("successfully resetted");
         },
         error: function(error) {
@@ -40,7 +47,8 @@ function resetData() {
     });
 }
 
-function getPhoneData() {
+// grabs the data that is stored in our api through an ajax request
+function getPhoneDataFromApi() {
     $.ajax({
         url: "https://wt.ops.labs.vu.nl/api20/2b9115a4",
         context: document.body,
@@ -51,12 +59,14 @@ function getPhoneData() {
     });
 }
 
-function populateTable(phonesData) {
-    $.each(phonesData, function(idx, phone) {
+// populates table with given data
+function populateTable(data) {
+    $.each(data, function(idx, phone) {
         addPhoneToTable(phone);
     });
 }
 
+//add new tablerow with given data
 function addPhoneToTable(data) {
     var table = $("#phonesTable").DataTable();
     table.row
@@ -68,34 +78,4 @@ function addPhoneToTable(data) {
             screensize: data.screensize
         })
         .draw();
-}
-
-function createPhone() {
-    var data = $("#phonesForm").serializeArray();
-
-    $.ajax({
-        url: "https://wt.ops.labs.vu.nl/api20/2b9115a4",
-        data: {
-            image: data[1].value,
-            brand: data[2].value,
-            model: data[3].value,
-            os: data[4].value,
-            screensize: data[5].value
-        },
-        method: "POST",
-        success: function(result) {
-            data = {
-                image: data[1].value,
-                brand: data[2].value,
-                model: data[3].value,
-                os: data[4].value,
-                screensize: data[5].value
-            };
-            addPhoneToTable(data);
-            alert(data.brand + " successfully added");
-        },
-        error: function(error) {
-            alert(error.responseText);
-        }
-    });
 }
